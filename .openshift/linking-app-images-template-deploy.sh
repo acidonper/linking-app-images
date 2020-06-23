efd60784949c937972fcd7e3396fa2e845e5e6a6
Asier Cidon <asier.cidon@gmail.com>
2020-06-23 17:12:14 +0200

    Added some ocp deployment changes

--- .openshift/linking-app-images-template-deploy.sh
+++ .openshift/linking-app-images-template-deploy.sh
@@ -43,7 +43,7 @@ oc process -f linking-app-images-template.yaml  \
 oc start-build bc/$SERVICE_NAME
 
 # Wait for previous process
-sleep 900
+#sleep 900
 
 # Start deployment process
-oc rollout latest $SERVICE_NAME -n $PROJECT_NAME
+#oc rollout latest $SERVICE_NAME -n $PROJECT_NAME
